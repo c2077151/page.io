2d01ffdf5e8b623f1b88ca58e01dcb667dd8c3b5
c2077151 <117455975+c2077151@users.noreply.github.com>
2023-01-11 11:14:01 +0000

    Add files via upload

---
+++ CantorCollege/js/script.js
@@ -0,0 +1,78 @@
+function changeWifi()
+{
+    var img = document.getElementById("facility");
+    img.src = "images/wifi.jpeg";
+    img.alt = "Students gathered around a computer";
+}
+function changeClassroom()
+{
+    var img = document.getElementById("facility");
+    img.src = "images/classroom.jpeg";
+    img.alt = "Students gathered around a table";
+}
+function changeFaculty()
+{
+    var img = document.getElementById("facility");
+    img.src = "images/facilities.jpg";
+    img.alt = "Faculty staff";
+}
+function changeLecture()
+{
+    var img = document.getElementById("facility");
+    img.src = "images/lecture.jpg";
+    img.alt = "An empty large lecture hall";
+}
+function changeAtrium()
+{
+    var img = document.getElementById("facility");
+    img.src = "images/atrium.jpg";
+    img.alt = "The main atrium";
+}
+function changeMeeting()
+{
+    var img = document.getElementById("facility");
+    img.src = "images/meeting.JPG";
+    img.alt = "An empty meeting room";
+}
+function changeOffice()
+{
+    var img = document.getElementById("facility");
+    img.src = "images/office.jpg";
+    img.alt = "An empty office space";
+}
+function changeIT()
+{
+    var img = document.getElementById("facility");
+    img.src = "images/it.jpg";
+    img.alt = "The hallam help logo";
+}
+function changeReception()
+{
+    var img = document.getElementById("facility");
+    img.src = "images/reception.jpg";
+    img.alt = "A reception";
+}
+function changeCatering()
+{
+    var img = document.getElementById("facility");
+    img.src = "images/catering.jpg";
+    img.alt = "A caffeteria";
+}
+function changeParking()
+{
+    var img = document.getElementById("facility");
+    img.src = "images/parking.jpg";
+    img.alt = "The road outside cantor building";
+}
+function changeBikes()
+{
+    var img = document.getElementById("facility");
+    img.src = "images/bike.jpg";
+    img.alt = "A row of parked bikes";
+}
+function changeGallery()
+{
+    var img = document.getElementById("facility");
+    img.src = "images/gallery.jpeg";
+    img.alt = "A gallery with six paintings";
+}
